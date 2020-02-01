04c37c2cd5260ffe0e7b49a5bb26ebe2717fa693
EDG-161 <44904881+EDG-161@users.noreply.github.com>
2020-02-01 15:49:25 -0600

    static

--- FaceVerify/models.py
+++ FaceVerify/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class AlbumImage(models.Model):
     album = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='static/images/')
 
     def __unicode__(self,):
         return str(self.image)
