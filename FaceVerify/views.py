5af820b4dd568aef2a1e89452ef4f9a8badbad60
EDG-161 <44904881+EDG-161@users.noreply.github.com>
2020-01-31 23:34:50 -0600

    subida

--- FaceVerify/views.py
+++ FaceVerify/views.py
@@ -25,24 +25,13 @@ from django.core.files.base import ContentFile
 # Create your views here. 
 def upload_image_view(request):
 	if request.method == 'POST':
-		print(request.FILES)
 		img_base64 = request.POST["img"]
 		data = ContentFile(b64decode(img_base64), 'whatup.png')
-		print (data)
 		newImage = AlbumImage()
 		newImage.image = data
 		newImage.album = "as"
 		newImage.save() 
-		form = UploadImageForm(request.POST, request.FILES)
-		
-		if form.is_valid():
-			message = "Image uploaded succesfully!"
-			form.save()
-			
-		else:
-			message="no se pudo"
-			form = UploadImageForm()
-
+		message = "Image uploaded succesfully!"
 	return HttpResponse(message) 
 
 def connection(request):
