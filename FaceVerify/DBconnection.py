263a7da6724ab315b45b56bbd273a2b534ac7c0d
EDG-161 <44904881+EDG-161@users.noreply.github.com>
2020-02-02 02:22:13 -0600

    co

--- FaceVerify/DBconnection.py
+++ FaceVerify/DBconnection.py
@@ -8,8 +8,8 @@ def vefUser(email,password):
 def login(email,password):
     db = pymysql.connect("gautabases.ga","usedoc_user","Use_pass223856220","usedoc")
     cursor = db.cursor()
-    cemail = Cipher.encrypt(email)
-    cpassword = Cipher.encrypt(password)
+    cemail = Cipher.encrypt(email)[0:]
+    cpassword = Cipher.encrypt(password)[0:]
     print (cemail)
     print(cpassword)
     sql = "SELECT * FROM musuarios where email_usr={} ".format(cemail)
