7bd07d762d3845330da8263d064ed9839921690c
EDG-161 <44904881+EDG-161@users.noreply.github.com>
2020-02-02 02:00:15 -0600

    a

--- FaceVerify/views.py
+++ FaceVerify/views.py
@@ -56,9 +56,9 @@ def verify_session(request):
 		return HttpResponse("error")
 
 def login(request):
-	try:
-		email = request.POST['email']
-		password = request.POST['pass']
-		return HttpResponse(DBconnection.login(email,password))
-	except:
-		return HttpResponse("Error")
+	#try:
+	email = request.POST['email']
+	password = request.POST['pass']
+	return HttpResponse(DBconnection.login(email,password))
+	#except:
+	#	return HttpResponse("Error")
