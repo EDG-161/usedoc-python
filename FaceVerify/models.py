3f4b3c9faec763946d6ebd62a4e1dfd7e8e351e1
EDG-161 <44904881+EDG-161@users.noreply.github.com>
2020-03-09 11:41:01 -0600

    fc

--- FaceVerify/models.py
+++ FaceVerify/models.py
@@ -3,7 +3,7 @@ from FaceVerify.storage import OverwriteStorage
 
 class AlbumImage(models.Model):
     album = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='static/static/images/',storage=OverwriteStorage())
+    image = models.ImageField(max_length=15000, storage=OverwriteStorage(),upload_to='static/static/images/')
 
     def __unicode__(self,):
         return str(self.image)
